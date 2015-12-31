dce5b2e838e93deb6f155e0ec925aa8581a14f0c
Cyrus Rhett Walker II <rhettwalker86@gmail.com>
2015-12-31 13:09:38 -0500

    Created initial tests for served client, Added public middleware, Created more tasks for Client side development

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 * bin/www was made executable with '>chmod +x bin/www'
 
+* Start Application with '>npm start'
+
 ## Polymer driven Express App 
 ### Serve the Application with Express
 - [x] Develop a testing environment with mocha
-- [ ] Design the shell folder structure for client app
+- [x] Design the shell folder structure for client app
 - [ ] Develop Express Web Server with continuous intergration
 - [ ] 
 - [ ] 
 
-### TODO :
-- [ ] 
-- [ ] 
-- [ ] 
+### Design the Shell of the Polymer SPA 
+- [ ] Install bower package manager
+- [ ] Create initial file directory structure
+- [ ] Develop AJAX calls to server 'cities/i'
 - [ ] 
 - [ ] 
 
--- test.js
+++ test.js
@@ -4,17 +4,50 @@ var app = require('./app');
 describe('Request to the root path', function(){
 
   it('Returns a 200 status code', function(done){
-    //Super Test Runner
- 
-  request(app)
-    .get('/')
-    .expect(200)
-    .end(function(error) {
-      if(error) throw error;
-      // test Mocha tests are done
-      done();
-    });
+   //Super Test Runner
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(error) {
+        if(error) throw error;
+        // test Mocha tests are done
+        done();
+      });
  
   });
 });
 
+describe('Listing cities on /cities', function(){
+
+  it('Returns 200 status code', function(done){
+    request(app)
+      .get('/cities')
+      .expect(200, done)
+  });
+
+  // Test that html is returned
+  it('Returns a HTML format', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done)
+  });
+
+  // Test for the body to match a call to /cities/i
+  it('Returns an index file with Cities', function(done) {
+    request(app)
+      .get('/')
+      .expect(/cities/i, done);
+  });
+
+  it('Returns JSON format', function(done) {
+    request(app)
+      .get('/cities')
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('Returns initial cities', function(done){
+    request(app)
+      .get('/cities')
+      .expect(JSON.stringify(['New York','Miami','Denver']), done);
+  });
+});
