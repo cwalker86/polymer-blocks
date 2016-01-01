c6b670a673c509f59e6877cc32bdf809b9a66d48
Cyrus Rhett Walker II <rhettwalker86@gmail.com>
2016-01-01 16:13:14 -0500

    Updated List page component to display proper response body

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.get('/cities', function(req, res) {
-  var cities = ['New York', 'Miami', 'Denver'];
+  var cities = ['New York', 'Miami', 'Denver', 'Seoul', 'Richmond'];
   res.json(cities);
 });
 
